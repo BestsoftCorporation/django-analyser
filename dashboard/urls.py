1f9c38559e35110f2e12e69cd36ea2f93dddafce
BestsoftCorporation <software.developer@hotmailrs>
2021-01-26 23:54:59 +0100

    Products and users table

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'dashboard'
 urlpatterns = [
     path('',views.home, name='home'),
+    path('products',views.products, name='products'),
+    path('users',views.users, name='users'),
 
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from .models import Products,Users
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'index.html', {'page_title': 'Dashboard'})
-# Create your views here.
+        return render(request, 'index.html', {'page_title': 'Dashboard'})
+def products(request):
+    pro = Products.objects.all()
+    return render(request, 'products.html', {'page_title': 'Dashboard','Products': pro})
+
+def users(request):
+    usr = Users.objects.all()
+    return render(request, 'users.html', {'page_title': 'Dashboard','Users': usr})
+
+
+
