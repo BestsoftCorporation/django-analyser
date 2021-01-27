43d54a1b97ffae13f5645b4c4ae5ab8ddf8b17d3
BestsoftCorporation <software.developer@hotmailrs>
2021-01-27 02:40:21 +0100

    Price filter

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,3 +3,6 @@ import django.forms as f
 
 class SearchForm(f.Form):
     search = f.CharField(widget=f.TextInput(attrs={'placeholder':'Search for...','class':'form-control'}))
+    price = f.CharField(widget=f.TextInput(attrs={'placeholder': 'Price', 'class': 'form-control'}))
+    option_list = [(1,'<') ,(2,'>'), (3,'=')]
+    option = f.ChoiceField(choices=option_list)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Products,Users
 from .forms import SearchForm
+from django.db.models import Avg,Count,Sum
 
 
 def home(request):
@@ -9,12 +10,18 @@ def products(request):
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
-            pro = Products.objects.filter(name__icontains=form.cleaned_data['search'])
-            return render(request, 'products.html', {'page_title': 'Dashboard', 'Products': pro, 'form': form})
+            pro = Products.objects.filter(name__icontains=form.cleaned_data['search']) & Products.objects.filter(price__gt=form.cleaned_data['price'])
+            avgPrice = pro.aggregate(Avg('price'))
+            countPrice = pro.aggregate(Count('id'))
+            sumPrice = pro.aggregate(Sum('price'))
+            return render(request, 'products.html', {'page_title': 'Dashboard', 'Products': pro,'SumPrice':sumPrice,'AvgPrice':avgPrice,'CountPrice':countPrice, 'form': form})
     else:
         form = SearchForm()
         pro = Products.objects.all()
-        return render(request, 'products.html', {'page_title': 'Dashboard','Products': pro,'form': form})
+        avgPrice=pro.aggregate(Avg('price'))
+        countPrice = pro.aggregate(Count('id'))
+        sumPrice = pro.aggregate(Sum('price'))
+        return render(request, 'products.html', {'page_title': 'Dashboard','Products': pro,'SumPrice':sumPrice,'AvgPrice':avgPrice,'CountPrice':countPrice,'form': form})
 
 def users(request):
     if request.method == 'POST':
